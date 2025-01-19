f50af136cb34eca2f5dd1b0623cf71a27297b0b5
kadudev <kadujunior567@gmail.com>
2025-01-18 20:18:10 -0400

    project

---
+++ js/aula02/script.js
@@ -0,0 +1,34 @@
+// tipos primitivos
+
+// Boolean
+// var VouF = false;
+// console.log(typeof VouF);
+
+// var numeroQualquer = 1;
+// console.log(typeof numeroQualquer);
+
+// var variavel = "kadu";
+// variavel = "junior";
+// console.log(variavel);
+
+// let variavel2 = "Carlos";
+// console.log(variavel2);
+
+// const constante = "Carlos";
+// console.log(constante);
+
+// var escopoGlobal = "global";
+// console.log(escopoGlobal);
+
+// function escopoLocal() {
+//   let escopoLocalInterno = "local";
+//   console.log(escopoLocalInterno);
+// }
+
+// escopoLocal();
+
+var comparacao = 0 == 0;
+console.log(comparacao);
+
+var comparacaoIdentica = 0 === 0;
+console.log(comparacaoIdentica);
---
+++ js/aula03-array-e-object/script.js
@@ -0,0 +1,35 @@
+// como declarar um array
+
+// let array = ['string', 1, true]
+// console.log(array)
+
+let array = ["string", 1, true, ["array1"], ["array2"], ["array3"], ["array4"]];
+
+// console.log(array[3])
+
+// forEach
+
+// array.forEach(function (item, index) {
+//   console.log(item, index)});
+
+// array.push("novo item");
+// console.log(array);
+
+// array.pop()
+// console.log(array)
+
+// array.shift()
+// console.log(array)
+
+// array.unshift("novo item");
+// console.log(array);
+
+
+// console.log(array.indexOf(true))
+
+// array.splice(0, 3)
+// console.log(array)
+
+
+let novoArray = array.slice(0, 3)
+console.log(array)
